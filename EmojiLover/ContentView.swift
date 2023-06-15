a3ee846e703a433e0610fc45668471b21982af4d
aaronsiim <web3aaron@gmail.com>
2023-06-15 13:25:26 -0400

    First commit, IOS emoji app.

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+// Enum = enumuration, title = emoji, type value string, caseiterable is protocol helps loops over the string
+enum Emoji: String, CaseIterable {
+    case 🇨🇦, 🇺🇸, 🇪🇸, 🇳🇵
+}
+
+// Extended to view
 struct ContentView: View {
+    // Selection is special type of data which is marked as state
+    // State helps update
+    @State var selection: Emoji = .🇺🇸
+    
+    // Body here for view
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        // Outer navigation view which has a title of Emoji lovers
+        NavigationView {
+            // Vertical stack container
+            VStack {
+                // Two elements in containers
+                // First = label (text) - shows current selection which is raw value giving string variale from emojis
+                // Second = Font to make emoji look larger
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                // Picker with a title  with state binding
+                // Looping over cases for all emoji with id = self
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                // You can change the picker stype as you wish
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            // Give padding for the picker
+            .padding()
         }
-        .padding()
     }
 }
 
