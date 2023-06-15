60e68879cd1f30bfeb5f9d615eeac92a6195254f
aaronsiim <web3aaron@gmail.com>
2023-06-15 12:36:43 -0400

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Aaron Sim on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
